a0c1bfdc3efeacc2b5680b81faeb786d86f46228
abdulkhadar.s <abdulkhadar.s@zohocorp.com>
2022-11-11 11:35:01 +0530

    Add new file

---
+++ resources/task2/ptactise/pytest.py
@@ -0,0 +1,15 @@
+'''
+def add(x,y):
+	return x+y
+def product(x,y):
+	return x*y
+def test_add():
+	assert add(7,3)==10
+def test_product():
+	assert product(5,2)==10
+'''
+def sum(a,b):
+	return a+b
+def test():
+	assert sum(5,6)==11
+	
